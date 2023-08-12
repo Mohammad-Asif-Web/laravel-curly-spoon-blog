f9760311aa766750d7ffd57e6403e2dc4ebf2414
muhammad asif <alexasifhhossain@gmail.com>
2023-08-12 20:30:06 +0600

    js file updated

--- public/Backend/js/datatables-simple-demo.js
+++ public/Backend/js/datatables-simple-demo.js
@@ -6,4 +6,35 @@ window.addEventListener('DOMContentLoaded', event => {
     if (datatablesSimple) {
         new simpleDatatables.DataTable(datatablesSimple);
     }
+
+    // user management data table
+    const userManagementData = document.getElementById('userManagementData');
+    if(userManagementData){
+        new simpleDatatables.DataTable(userManagementData);
+    }
+
+    // post engagement data table
+    const postEngagementData = document.getElementById('postEngagementData');
+    if(postEngagementData){
+        new simpleDatatables.DataTable(postEngagementData);
+    }
+
+    // popular posts
+    const popularPostData = document.getElementById('popularPostData');
+    if(popularPostData){
+        new simpleDatatables.DataTable(popularPostData);
+    }
+
+    // recent activity
+    const recentActivityData = document.getElementById('recentActivityData');
+    if(recentActivityData){
+        new simpleDatatables.DataTable(recentActivityData);
+    }
+
+
 });
+
+
+
+
+
