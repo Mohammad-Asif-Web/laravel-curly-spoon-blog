c16055f34e2ba01f109cf4197cb0475e618eea6d
muhammad asif <alexasifhhossain@gmail.com>
2023-08-12 23:41:12 +0600

    tag router added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Backend\BackendController;
 use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\TagController;
 use App\Http\Controllers\Frontend\FrontendController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -44,4 +45,5 @@ Route::get('contact', [FrontendController::class, 'contact'])->name('front.conta
 Route::group(['prefix'=>'dashboard'], function(){
     Route::get('/', [BackendController::class, 'index'])->name('back.index');
     Route::resource('category', CategoryController::class);
+    Route::resource('tag', TagController::class);
 });
